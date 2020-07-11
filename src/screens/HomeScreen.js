eac0acaf1368801badab4e175a6c21b48d093986
unknown <daler@gmail.com>
2020-07-11 14:41:27 +0500

    git init

---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Text, StyleSheet } from "react-native";
+
+const HomeScreen = () => {
+  return <Text style={styles.text}>Mobile Application motherfuc*er, ha ha ha</Text>;
+};
+
+const styles = StyleSheet.create({
+  text: {
+    fontSize: 30
+  }
+});
+
+export default HomeScreen;
